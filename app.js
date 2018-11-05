c76c25931e509cd4980ef357cca098779feff64f
t-kojima <t-kojima@skullware.net>
2018-11-05 10:46:49 +0900

    feat: 設定をjsonから読み込む

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const log4js = require('log4js');
+
+log4js.configure('./log4js.json');
 const logger = log4js.getLogger();
 
-logger.level = 'info';
+app.use(log4js.connectLogger(log4js.getLogger('express')));
 
-app.get('/', (req, res) => res.send("hello world!"));
+app.get('/', (req, res) => res.send('hello world!'));
 
 app.get('/:name', (req, res) => {
-  res.header('Content-Type', 'application/json; charset=utf-8')
-  res.send({ message: `hello ${req.params.name}` })
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send({ message: `hello ${req.params.name}` });
   logger.info(`hello ${req.params.name}`);
-})
+});
 
-app.listen(3000, () => console.log("http://localhost:3000"))
+app.listen(3000, () => console.log('http://localhost:3000'));
